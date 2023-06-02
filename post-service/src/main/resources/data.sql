59223634f6c80e7271df83fcc07bc19eab8e58cc
Uladzislau_Valiavaty <Uladzislau_Valiavaty@epam.com>
2023-06-02 23:03:18 +0400

    Module 3. Task 1.

--- post-service/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO posts (author_id, posted_at, "text") VALUES(1, '2023-04-28 12:34:25', 'example text 1');
-INSERT INTO posts (author_id, posted_at, "text") VALUES(2, '2023-04-28 12:35:25', 'example text 2');
-INSERT INTO posts (author_id, posted_at, "text") VALUES(3, '2023-04-28 12:36:25', 'example text 3');
-INSERT INTO posts (author_id, posted_at, "text") VALUES(3, '2023-04-28 12:37:25', 'example text 4');
-INSERT INTO posts (author_id, posted_at, "text") VALUES(3, '2023-04-28 12:38:25', 'example text 5');
--- user-service/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (username, amount_of_posts) VALUES('user-1', 1);
-INSERT INTO users (username, amount_of_posts) VALUES('user-2', 1);
-INSERT INTO users (username, amount_of_posts) VALUES('user-3', 3);
